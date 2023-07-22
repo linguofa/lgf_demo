e1b172c739a8c49a3f98d5dbcd17e96c6f5d9327
lgf <453326561@qq.com>
2023-07-22 19:51:34 +0800

    最后-第一次
    最后-第2次

--- irma-driver/tets1.py
+++ irma-driver/tets1.py
@@ -1,5 +1,11 @@
 #1.切换到开发分支git pull -r
-#2.如果提示有未修改的话，就add，commit再执行上面命令
-#3.开发啊开发啊，然后add commit，开发完之后，就push上远程
-#然后执行合并commmit，准备合并到正式环境
-#git rebase -i HEAD~2git
+#2.然后add commit，开发完之后，就push上远程，本地和远程同步了
+#3.然后准备执行合并commmit，准备合并到正式环境
+#4.git rebase -i HEAD~2
+#5.如果有意外就git rebase --quit取消合并
+#6.记录下来commit的id
+
+#7.去到master，先git pull -r
+#8.然后git cherry-pick comit的id
+#9.然后git pr 吧，或者git push
+
